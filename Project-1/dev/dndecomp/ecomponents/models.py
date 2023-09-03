e02597345551cbb1c85317e8609ce9d1041dd0f2
Engr. Bryndell Torio <bryndell.torio@global-imi.com>
2023-09-03 09:43:58 +0800

    Default.png image added for user registration.

--- Project-1/dev/dndecomp/ecomponents/models.py
+++ Project-1/dev/dndecomp/ecomponents/models.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
 
-    profile_pic = models.ImageField(null=True, blank=True, default='')
+    profile_pic = models.ImageField(null=True, blank=True, default='Default.png')
 
     # FK
 

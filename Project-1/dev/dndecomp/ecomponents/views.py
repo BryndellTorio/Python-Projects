f4efec9066f08a991738e4b7439ff278de4e97bc
Engr. Bryndell Torio <bryndell.torio@global-imi.com>
2023-09-09 17:19:02 +0800

    Udemy step 25: able to run the web page though under 127.0.0.1:8000/register the layout is different from the one shown in the tutorial. Maybe we can get more information once we get to the next steps.

--- Project-1/dev/dndecomp/ecomponents/views.py
+++ Project-1/dev/dndecomp/ecomponents/views.py
@@ -1,23 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+
+from .forms import CreateUserForm
+
 
 def index(request):
+    return render(request, "ecomponents/index.html")
 
-    return render(request, 'ecomponents/index.html')
 
 def register(request):
+    form = CreateUserForm()
 
-    return render(request, 'ecomponents/register.html')
+    if request.method == "POST":
+        form = CreateUserForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+
+            return redirect("my-login")
+
+    context = {"form": form}
+
+    return render(request, "ecomponents/register.html", context=context)
+
 
 def my_login(request):
+    return render(request, "ecomponents/my-login.html")
 
-    return render(request, 'ecomponents/my-login.html')
 
 def dashboard(request):
+    return render(request, "ecomponents/dashboard.html")
 
-    return render(request, 'ecomponents/dashboard.html')
 
 def profile_management(request):
-
-    return render(request, 'ecomponents/profile-management.html')
+    return render(request, "ecomponents/profile-management.html")
